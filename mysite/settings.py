bd8336095ee3e9e4c294280f868660de18776276
Lotfy <lotfy1105@gmail.com>
2023-10-06 21:10:08 +0300

    Using Django Forms To Share Posts Via Email

--- mysite/settings.py
+++ mysite/settings.py
@@ -122,3 +122,13 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+# Email server configuration
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_USE_SSL = False
+EMAIL_HOST_USER = 'sniperqa10@gmail.com'
+EMAIL_HOST_PASSWORD = 'tclj ntso hjos cynh'
